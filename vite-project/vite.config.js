e23f36a637345019093a6b72e27b3cbfafe96262
Ashutosh Yadav <ashutosh2213072@akgec.ac.in>
2025-07-21 17:37:42 +0530

    Fix: set ROLLUP_BROWSER for Rollup on Vercel

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,6 @@
+process.env.ROLLUP_BROWSER = 'true';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
